14516a4d5ae6bf8be9261590e77afeb99688a0f9
Artem Vorotnikov <artem@vorotnikov.me>
2018-04-05 00:39:56 +0300

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,41 @@
+//! Convert between XML nodes ([treexml](https://github.com/rahulg/treexml-rs)) and JSON objects ([serde-json](https://github.com/serde-rs/json)).
+//!
+//! ## Example
+//! ```
+//! extern crate treexml;
+//!
+//! #[macro_use]
+//! extern crate serde_json;
+//!
+//! extern crate node2object;
+//!
+//! fn main() {
+//!     let dom_root = treexml::Document::parse("
+//!         <population>
+//!           <entry>
+//!             <name>Alex</name>
+//!             <height>173.5</height>
+//!           </entry>
+//!           <entry>
+//!             <name>Mel</name>
+//!             <height>180.4</height>
+//!           </entry>
+//!         </population>
+//!     ".as_bytes()).unwrap().root.unwrap();
+//!
+//!     assert_eq!(serde_json::Value::Object(node2object::node2object(&dom_root)), json!(
+//!         {
+//!           "population": {
+//!             "entry": [
+//!               { "name": "Alex", "height": 173.5 },
+//!               { "name": "Mel", "height": 180.4 }
+//!             ]
+//!           }
+//!         }
+//!     ));
+//! }
+//! ```
+
 extern crate treexml;
 
 #[macro_use]
@@ -119,6 +157,7 @@ fn convert_node_aux(e: &treexml::Element) -> Option<Value> {
     }
 }
 
+/// Converts treexml::Element into a serde_json hashmap. The latter can be wrapped in Value::Object.
 pub fn node2object(e: &treexml::Element) -> Map<String, Value> {
     let mut data = Map::new();
     data.insert(e.name.clone(), convert_node_aux(e).unwrap_or(Value::Null));
